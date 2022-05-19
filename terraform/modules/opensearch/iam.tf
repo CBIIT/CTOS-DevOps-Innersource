ba0e924beeddee51aa6498384dd2eeed3f458dbf
colemandevries <colemandevries@gmail.com>
2022-05-19 10:42:00 -0400

    Adding Opensearch IAM policies to support manual snapshot operations

---
+++ terraform/modules/opensearch/iam.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_role" "os" {
+  name               = "${var.app}-opensearch-snapshot-role-${terraform.workspace}"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "os" {
+  role       = aws_iam_role.os.name
+  policy_arn = data.aws_iam_policy_document.os.id
+}

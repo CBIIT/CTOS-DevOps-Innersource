e349907542f88083d20b4d3926d285b5bf0d7247
colemandevries <colemandevries@gmail.com>
2022-05-23 13:40:17 -0400

    hard-coding standard opensearch variables

--- terraform/modules/opensearch/variables.tf
+++ terraform/modules/opensearch/variables.tf
@@ -32,30 +32,24 @@ variable "ebs_volume_size" {
 
 variable "subnet_ids" {
   type = set(string)
+  description = "The subnets which OpenSearch will be deployed to"
 }
 
 variable "snapshot_hour" {
   type = number
-}
-
-variable "encrypt_rest" {
-  type = bool
-}
-
-variable "node_to_node_encryption" {
-  type = bool
-}
-
-variable "logs_enabled" {
-  type = bool
+  description = "The hour in which OpenSearch will perform automated snapshot operations"
+  default = 23
 }
 
 variable "log_type" {
   type = string
+  description = "The type of OpenSearch logs to forward to CloudWatch. Options include 'INDEX_SLOW_LOGS, 'SEARCH_SLOW_LOGS', 'ES_APPLICATION_LOGS', and 'AUDIT_LOGS'"
+  default = "INDEX_SLOW_LOGS"
 }
 
 variable "log_retention" {
   type = number
+  description = "The number of days to save OpenSearch logs sent to CloudWatch"
 }
 
 variable "vpc_id" {

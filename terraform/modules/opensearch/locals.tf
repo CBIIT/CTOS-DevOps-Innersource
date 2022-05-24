631565d6cb6e9e7ced897d2159da16e68c9a947f
colemandevries <colemandevries@gmail.com>
2022-05-24 06:58:29 -0400

    updated iam policy names to meet nci conditions

--- terraform/modules/opensearch/locals.tf
+++ terraform/modules/opensearch/locals.tf
@@ -6,6 +6,6 @@ locals {
   snapshot_bucket  = "${var.app}-opensearch-snapshots-${var.tier}"
   snapshot_role    = "${var.policy_prefix}-${var.app}-opensearch-snapshot-role-${var.tier}"
   snapshot_policy  = "${var.policy_prefix}-${var.app}-opensearch-snapshot-policy-${var.tier}"
-  jenkins_snapshot = "${var.policy_prefix}-${var.app}-jenkins-pass-role-policy-os-snapshot-${var.tier}"
+  jenkins_snapshot = "${var.policy_prefix}-${var.app}-jenkins-pass-role-os-snapshot-${var.tier}"
   permission_boundary_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/PermissionBoundary_PowerUser"
 }

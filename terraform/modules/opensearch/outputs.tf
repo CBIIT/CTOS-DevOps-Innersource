ec956d99ba4855885f986c7fecd1f61e4dc36b90
colemandevries <colemandevries@gmail.com>
2022-05-24 17:53:16 -0400

    Creating a new OpenSearch module

---
+++ terraform/modules/opensearch/outputs.tf
@@ -0,0 +1,15 @@
+output "opensearch_arn" {
+  value = aws_opensearch_domain.opensearch.arn
+}
+
+output "opensearch_domain_id" {
+  value = aws_opensearch_domain.opensearch.domain_id
+}
+
+output "opensearch_domain_name" {
+  value = aws_opensearch_domain.opensearch.domain_name
+}
+
+output "opensearch_endpoint" {
+  value = aws_opensearch_domain.opensearch.endpoint
+}
---
+++ terraform/modules/opensearch/variables.tf
@@ -0,0 +1,62 @@
+variable "app" {
+  type = string
+}
+
+variable "tier" {
+  type = string
+}
+
+variable "engine_version" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "data_node_count" {
+  type = number
+}
+
+variable "multi-az" {
+  type = bool
+}
+
+variable "ebs_enabled" {
+  type = bool
+}
+
+variable "ebs_volume_size" {
+  type = number
+}
+
+variable "subnet_ids" {
+  type = set(string)
+  description = "The subnets which OpenSearch will be deployed to"
+}
+
+variable "snapshot_hour" {
+  type = number
+  description = "The hour in which OpenSearch will perform automated snapshot operations"
+  default = 23
+}
+
+variable "log_type" {
+  type = string
+  description = "The type of OpenSearch logs to forward to CloudWatch. Options include 'INDEX_SLOW_LOGS, 'SEARCH_SLOW_LOGS', 'ES_APPLICATION_LOGS', and 'AUDIT_LOGS'"
+  default = "INDEX_SLOW_LOGS"
+}
+
+variable "log_retention" {
+  type = number
+  description = "The number of days to save OpenSearch logs sent to CloudWatch"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "iam_prefix" {
+  type = string 
+  default = "power-user"
+}

b04185d15ded4ae6713e0ede9432ff4f498b048b
colemandevries <colemandevries@gmail.com>
2022-05-25 13:33:04 -0400

    conditional expression to use wildcard cert based on value of use_wildcard_cert variable

--- terraform/modules/alb/acm.tf
+++ terraform/modules/alb/acm.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_listener_certificate" "alb" {
   count = var.use_wildcard_cert == true ? 1 : 0
-  
+
   listener_arn    = aws_lb_listener.https.arn
   certificate_arn = data.aws_acm_certificate.wildcard.arn
 }

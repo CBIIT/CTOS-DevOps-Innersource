2eb9a1392277585dec1441441b869df9efef3bea
Sowmya Karavadi <ksowmya442@gmail.com>
2022-05-27 16:25:17 -0400

    updated terraform/modules/secretsmanager/secretsmanager.tf

--- terraform/modules/secretsmanager/secretsmanager.tf
+++ terraform/modules/secretsmanager/secretsmanager.tf
@@ -1,13 +1,11 @@
 resource "aws_secretsmanager_secret" "secrets_manager" {
-  for_each = var.secrets
-  name     = each.key
+  name        = local.secret_manager_name
   description = "Managed by Terraform"
 }
 
 resource "aws_secretsmanager_secret_version" "secret" {
-  for_each = var.secrets
-  secret_id     = each.key
-  secret_string = jsonencode(each.value)
+  secret_id     = aws_secretsmanager_secret.secrets_manager.id
+  secret_string = jsonencode(var.secrets)
   depends_on = [
     aws_secretsmanager_secret.secrets_manager
   ]

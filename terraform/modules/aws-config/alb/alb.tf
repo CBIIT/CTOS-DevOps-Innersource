815935abd13c29405a0912389fa217d65f899b37
Cole DeVries <colemandevries@gmail.com>
2022-06-05 10:52:30 -0400

    added descriptions for alb config rules

--- terraform/modules/aws-config/alb/alb.tf
+++ terraform/modules/aws-config/alb/alb.tf
@@ -2,10 +2,11 @@
 ### ELB_CROSS_ZONE_LOAD_BALANCING_ENABLED
 ### ELB_CUSTOM_SECURITY_POLICY_SSL_CHECK
 ### ELB_PREDEFINED_SECURITY_POLICY_SSL_CHECK
+### ELB_ACM_CERTIFICATE_REQUIRED
 
 resource "aws_config_config_rule" "alb_desync_mode_check" {
   name = "alb-desync-mode-check"
-  description = ""
+  description = "Checks if an Application Load Balancer (ALB) is configured with a user defined desync mitigation mode. The rule is NON_COMPLIANT if ALB desync mitigation mode does not match with the user defined desync mitigation mode."
 
   source {
     owner             = "AWS"
@@ -15,7 +16,7 @@ resource "aws_config_config_rule" "alb_desync_mode_check" {
 
 resource "aws_config_config_rule" "alb_http_drop_invalid_header_enabled" {
   name = "alb-http-drop-invalid-header-enabled"
-  description = ""
+  description = "Checks if rule evaluates AWS Application Load Balancers (ALB) to ensure they are configured to drop http headers. The rule is NON_COMPLIANT if the value of routing.http.drop_invalid_header_fields.enabled is set to false."
 
   source {
     owner             = "AWS"
@@ -25,7 +26,7 @@ resource "aws_config_config_rule" "alb_http_drop_invalid_header_enabled" {
 
 resource "aws_config_config_rule" "alb_http_to_https_redirection_check" {
   name = "alb-http-to-https-redirection-check"
-  description = ""
+  description = "Checks if HTTP to HTTPS redirection is configured on all HTTP listeners of Application Load Balancers. The rule is NON_COMPLIANT if one or more HTTP listeners of Application Load Balancer do not have HTTP to HTTPS redirection configured. The rule is also NON_COMPLIANT if one of more HTTP listeners have forwarding to an HTTP listener instead of redirection."
 
   source {
     owner             = "AWS"
@@ -35,7 +36,7 @@ resource "aws_config_config_rule" "alb_http_to_https_redirection_check" {
 
 resource "aws_config_config_rule" "alb_waf_enabled" {
   name = "alb-waf-enabled"
-  description = ""
+  description = "Checks if Web Application Firewall (WAF) is enabled on Application Load Balancers (ALBs). This rule is NON_COMPLIANT if key: waf.enabled is set to false."
 
   source {
     owner             = "AWS"
@@ -43,19 +44,9 @@ resource "aws_config_config_rule" "alb_waf_enabled" {
   }
 }
 
-resource "aws_config_config_rule" "elbv2_acm_certificate_required" {
-  name = "elbv2-acm-certificate-required"
-  description = ""
-
-  source {
-    owner             = "AWS"
-    source_identifier = "ELBV2_ACM_CERTIFICATE_REQUIRED"
-  }
-}
-
 resource "aws_config_config_rule" "elbv2_multiple_az" {
   name = "elbv2-multiple-az"
-  description = ""
+  description = "Checks if an Elastic Load Balancer V2 (Application, Network, or Gateway Load Balancer) has registered instances from multiple Availability Zones (AZ's). The rule is NON_COMPLIANT if an Elastic Load Balancer V2 has instances registered in less than 2 AZ's."
 
   source {
     owner             = "AWS"
@@ -63,9 +54,10 @@ resource "aws_config_config_rule" "elbv2_multiple_az" {
   }
 }
 
+
 resource "aws_config_config_rule" "elb_deletion_protection_enabled" {
   name = "elb-deletion-protection-enabled"
-  description = ""
+  description = "Checks if Elastic Load Balancing has deletion protection enabled. The rule is NON_COMPLIANT if deletion_protection.enabled is false."
 
   source {
     owner             = "AWS"
@@ -75,7 +67,7 @@ resource "aws_config_config_rule" "elb_deletion_protection_enabled" {
 
 resource "aws_config_config_rule" "elb_logging_enabled" {
   name = "elb-logging-enabled"
-  description = ""
+  description = "Checks if the Application Load Balancer and the Classic Load Balancer have logging enabled. The rule is NON_COMPLIANT if the access_logs.s3.enabled is false or access_logs.S3.bucket is not equal to the s3BucketName that you provided."
 
   source {
     owner             = "AWS"

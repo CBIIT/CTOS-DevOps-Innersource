22a494d5fdbaadece48626686c0d75d33c024a05
colemandevries <colemandevries@gmail.com>
2022-06-06 16:04:18 -0400

    removed remaining data.tf and iam.tf resources no longer needed

--- terraform/modules/opensearch-manual-snapshot/data.tf
+++ terraform/modules/opensearch-manual-snapshot/data.tf
@@ -5,18 +5,4 @@ data "aws_vpc" "vpc" {
   id = var.vpc_id
 }
 
-data "aws_instance" "jenkins_host" {
-  instance_id = var.jenkins_host_id
-}
-
-data "aws_iam_policy_document" "os_sts" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["es.amazonaws.com"]
-    }
-  }
-}
 

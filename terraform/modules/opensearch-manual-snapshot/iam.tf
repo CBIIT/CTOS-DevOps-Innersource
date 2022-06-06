9b489313a33a129ae01766a3e24cd52336203015
colemandevries <colemandevries@gmail.com>
2022-06-06 16:02:37 -0400

    removed the iam policy attachment resources and the os_snapshot policy resource from iam.tf

--- terraform/modules/opensearch-manual-snapshot/iam.tf
+++ terraform/modules/opensearch-manual-snapshot/iam.tf
@@ -8,16 +8,3 @@ resource "aws_iam_policy" "os_snapshot" {
   name   = local.snapshot_policy
   policy = data.aws_iam_policy_document.os_snapshot.json
 }
-
-resource "aws_iam_role_policy_attachment" "os_snapshot" {
-  role       = aws_iam_role.os_snapshot.name
-  policy_arn = aws_iam_policy.os_snapshot.arn
-}
-
-# To allow Jenkins Host to assume the snapshot role defined above.
-
-
-resource "aws_iam_role_policy_attachment" "jenkins_snapshot" {
-  role       = var.jenkins_profile_role_name
-  policy_arn = aws_iam_policy.jenkins_snapshot.arn
-}

1c01f1312dbf5ca72ce9433139ebd3953e5b2d6e
colemandevries <colemandevries@gmail.com>
2022-06-07 10:21:56 -0400

    added aws config recorder resource in module aws-config

---
+++ terraform/modules/aws-config/recorder/main.tf
@@ -0,0 +1,21 @@
+resource "aws_config_configuration_recorder" "recorder" {
+  name = "config-recorder"
+  role_arn = aws_iam_role.recorder.arn
+}
+
+resource "aws_iam_role" "recorder" {
+  name = "config-recorder-role"
+  assume_role_policy = data.aws_iam_policy_document.recorder.json
+}
+
+data "aws_iam_policy_document" "recorder" {
+  statement {
+    actions = [ "sts:AssumeRole" ]
+    effect = "Allow"
+    sid = "AwsConfigRecorder"
+    principals {
+      type = "AWS"
+      identifiers = [ "config.amazonaws.com" ]
+    }
+  }
+}

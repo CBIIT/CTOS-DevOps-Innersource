ab9260fc576485eb76ca62e22d71a8d45ff186ed
colemandevries <colemandevries@gmail.com>
2022-06-07 06:31:29 -0400

    updated alb security groups

--- terraform/modules/alb/locals.tf
+++ terraform/modules/alb/locals.tf
@@ -4,6 +4,4 @@ locals {
   alb_sg                       = "${var.app}-alb-${terraform.workspace}"
   log_bucket                   = "${var.app}-alb-access-logs-${terraform.workspace}"
   sg_description               = "The security group attached to the ${var.app}-alb-${var.tier} application load balancer"
-  sg_rule_inbound_description  = "Defines the https inbound rules for the ${var.app}-sg-${var.tier} application load balancer"
-  sg_rule_outbound_description = "Defines all outbound rules for the ${var.app}-sg-${var.tier} application load balancer"
 }

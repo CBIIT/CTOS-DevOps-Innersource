6b7fa893ad5cf4b4280b8fffe168ce6b38b4911b
colemandevries <colemandevries@gmail.com>
2022-06-07 08:12:04 -0400

    fixed opensearch config descriptions to be shorter

--- terraform/modules/aws-config/opensearch/main.tf
+++ terraform/modules/aws-config/opensearch/main.tf
@@ -12,7 +12,7 @@ resource "aws_config_config_rule" "opensearch_audit_logging_enabled" {
   count = var.opensearch_audit_logging_enabled == true ? 1 : 0
 
   name        = "opensearch-audit-logging-enabled"
-  description = "Checks if Amazon OpenSearch Service domains have audit logging enabled. The rule is NON_COMPLIANT if an OpenSearch Service domain does not have audit logging enabled. Triggered only when changes to OpenSearch resources are applied in an environment."
+  description = "Checks if Amazon OpenSearch Service domains have audit logging enabled. Triggered only when changes to OpenSearch resources are applied in an environment."
 
   source {
     owner             = "AWS"
@@ -24,7 +24,7 @@ resource "aws_config_config_rule" "opensearch_encrypted_at_rest" {
   count = var.opensearch_encrypted_at_rest == true ? 1 : 0
 
   name        = "opensearch-encrypted-at-rest"
-  description = "Checks if Amazon OpenSearch Service domains have encryption at rest configuration enabled. The rule is NON_COMPLIANT if EncryptionAtRestOptions field is not enabled. Triggered only when changes to OpenSearch resources are applied in an environment."
+  description = "Checks if Amazon OpenSearch Service domains have encryption at rest configuration enabled. Triggered only when changes to OpenSearch resources are applied in an environment."
 
   source {
     owner             = "AWS"
@@ -36,7 +36,7 @@ resource "aws_config_config_rule" "opensearch_https_required" {
   count = var.opensearch_https_required == true ? 1 : 0
 
   name        = "opensearch-https-required"
-  description = "Checks whether connections to OpenSearch domains are using HTTPS. The rule is NON_COMPLIANT if the Amazon OpenSearch domain 'EnforceHTTPS' is not 'true' or is 'true' and 'TLSSecurityPolicy' is not in 'tlsPolicies'. Triggered only when changes to OpenSearch resources are applied in an environment."
+  description = "Checks whether connections to OpenSearch domains are using HTTPS. Triggered only when changes to OpenSearch resources are applied in an environment."
 
   source {
     owner             = "AWS"
@@ -48,7 +48,7 @@ resource "aws_config_config_rule" "opensearch_in_vpc_only" {
   count = var.opensearch_in_vpc_only == true ? 1 : 0
 
   name        = "opensearch-in-vpc-only"
-  description = "Checks if Amazon OpenSearch Service domains are in an Amazon Virtual Private Cloud (VPC). The rule is NON_COMPLIANT if an OpenSearch Service domain endpoint is public. Triggered only when changes to OpenSearch resources are applied in an environment."
+  description = "Checks if Amazon OpenSearch Service domains are in an Amazon Virtual Private Cloud (VPC). Triggered only when changes to OpenSearch resources are applied in an environment."
 
   source {
     owner             = "AWS"
@@ -60,7 +60,7 @@ resource "aws_config_config_rule" "opensearch_logs_to_cloudwatch" {
   count = var.opensearch_logs_to_cloudwatch == true ? 1 : 0
 
   name        = "opensearch-logs-to-cloudwatch"
-  description = "Checks if Amazon OpenSearch Service domains are configured to send logs to Amazon CloudWatch Logs. The rule is NON_COMPLIANT if logging is not configured. Triggered only when changes to OpenSearch resources are applied in an environment."
+  description = "Checks if Amazon OpenSearch Service domains are configured to send logs to Amazon CloudWatch Logs. Triggered only when changes to OpenSearch resources are applied in an environment."
 
   source {
     owner             = "AWS"
@@ -72,7 +72,7 @@ resource "aws_config_config_rule" "opensearch_node_to_node_encryption_check" {
   count = var.opensearch_node_to_node_encryption_check == true ? 1 : 0
 
   name        = "opensearch-node-to-node-encryption-enabled"
-  description = "Check that Amazon OpenSearch Service nodes are encrypted end to end. The rule is NON_COMPLIANT if the node-to-node encryption is not enabled on the domain. Triggered only when changes to OpenSearch resources are applied in an environment."
+  description = "Check that Amazon OpenSearch Service nodes are encrypted end to end. Triggered only when changes to OpenSearch resources are applied in an environment."
 
   source {
     owner             = "AWS"

534935e8c4976a50411df737bf99f89b6e74374c
colemandevries <colemandevries@gmail.com>
2022-06-07 06:45:04 -0400

    removed last opensearch securitygroup rule from module

--- terraform/modules/alb/locals.tf
+++ terraform/modules/alb/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  alb_name                     = "${var.app}-alb-${terraform.workspace}"
-  prefix                       = "alb/${var.app}-${terraform.workspace}"
-  alb_sg                       = "${var.app}-alb-${terraform.workspace}"
-  log_bucket                   = "${var.app}-alb-access-logs-${terraform.workspace}"
-  sg_description               = "The security group attached to the ${var.app}-alb-${var.tier} application load balancer"
+  alb_name       = "${var.app}-alb-${terraform.workspace}"
+  prefix         = "alb/${var.app}-${terraform.workspace}"
+  alb_sg         = "${var.app}-alb-${terraform.workspace}"
+  log_bucket     = "${var.app}-alb-access-logs-${terraform.workspace}"
+  sg_description = "The security group attached to the ${var.app}-alb-${var.tier} application load balancer"
 }
--- terraform/modules/opensearch/securitygroups.tf
+++ terraform/modules/opensearch/securitygroups.tf
@@ -2,12 +2,3 @@ resource "aws_security_group" "opensearch" {
   name   = local.sg
   vpc_id = var.vpc_id
 }
-
-resource "aws_security_group_rule" "opensearch_inbound_https" {
-  security_group_id = aws_security_group.opensearch.id
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = [data.aws_vpc.vpc.cidr_block]
-}

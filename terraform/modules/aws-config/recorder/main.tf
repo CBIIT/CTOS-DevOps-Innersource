cd19d859ccce5eead99d71e717395b476140b525
Cole DeVries <colemandevries@gmail.com>
2022-06-08 05:56:19 -0400

    ran terraform fmt for all directories

--- terraform/modules/aws-config/recorder/main.tf
+++ terraform/modules/aws-config/recorder/main.tf
@@ -1,22 +1,22 @@
 resource "aws_config_configuration_recorder" "recorder" {
-  name = "config-recorder"
+  name     = "config-recorder"
   role_arn = aws_iam_role.recorder.arn
 }
 
 resource "aws_iam_role" "recorder" {
-  name = "${var.iam_prefix}-config-recorder-role"
-  assume_role_policy = data.aws_iam_policy_document.recorder.json
+  name                 = "${var.iam_prefix}-config-recorder-role"
+  assume_role_policy   = data.aws_iam_policy_document.recorder.json
   permissions_boundary = var.permission_boundary_arn
 }
 
 data "aws_iam_policy_document" "recorder" {
   statement {
-    actions = [ "sts:AssumeRole" ]
-    effect = "Allow"
-    sid = "AwsConfigRecorder"
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    sid     = "AwsConfigRecorder"
     principals {
-      type = "Service"
-      identifiers = [ "config.amazonaws.com" ]
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
     }
   }
 }

76d3d9c629ed9cd5494345149bf9b9015f70776c
Cole DeVries <colemandevries@gmail.com>
2022-06-12 16:08:09 -0400

    Formatting updated Terraform configuration files

--- terraform/modules/opensearch-manual-snapshot/outputs.tf
+++ terraform/modules/opensearch-manual-snapshot/outputs.tf
@@ -1,35 +1,35 @@
 output "opensearch_domain_id" {
-  value = aws_opensearch_domain.os.domain_id
+  value       = aws_opensearch_domain.os.domain_id
   description = "The ID of the OpenSearch domain"
 }
 
 output "opensearch_domain_name" {
-  value = aws_opensearch_domain.os.domain_name
+  value       = aws_opensearch_domain.os.domain_name
   description = "The domain name of the OpenSearch domain"
 }
 
 output "opensearch_endpoint" {
-  value = aws_opensearch_domain.os.endpoint
+  value       = aws_opensearch_domain.os.endpoint
   description = "The OpenSearch domain endpoint URL"
 }
 
 output "opensearch_engine_version" {
-  value = aws_opensearch_domain.os.engine_version
+  value       = aws_opensearch_domain.os.engine_version
   description = "The engine version of the OpenSearch domain (i.e. OpenSearch_1.2)"
 }
 
 output "opensearch_kibana_endpoint" {
-  value = aws_opensearch_domain.os.kibana_endpoint
+  value       = aws_opensearch_domain.os.kibana_endpoint
   description = "The OpenSearch domain dashboard endpoint URL"
 }
 
 output "s3_snapshot_bucket_name" {
-  value = aws_s3_bucket.opensearch_snapshot.bucket
+  value       = aws_s3_bucket.opensearch_snapshot.bucket
   description = "The name of the bucket that stores the OpenSearch domain manual snapshots"
 }
 
 output "s3_snapshot_bucket_arn" {
-  value = aws_s3_bucket.opensearch_snapshot.arn
+  value       = aws_s3_bucket.opensearch_snapshot.arn
   description = "The arn of the bucket that stores the OpenSearch domain manual snapshots"
 }
 

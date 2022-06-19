8e982aa3aefe9e819503f286dd7bd2d0e1624c1a
Cole DeVries <colemandevries@gmail.com>
2022-06-19 12:26:49 -0400

    added deployment circuit breaker to ecs module

--- terraform/modules/ecs/ecs.tf
+++ terraform/modules/ecs/ecs.tf
@@ -30,6 +30,11 @@ resource "aws_ecs_service" "ecs_service_frontend" {
     container_port   = var.frontend_port
   }
 
+  deployment_circuit_breaker {
+    enable = true 
+    rollback = true
+  }
+
   lifecycle {
     ignore_changes = [task_definition, desired_count]
   }

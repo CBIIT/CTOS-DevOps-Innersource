819069008d49dcd68b9ef5163a3d71da9f67d8f1
Sowmya Karavadi <ksowmya442@gmail.com>
2022-09-20 13:07:51 -0400

    updated ansible/collections/mtp/common/roles/build_frontend/files/inject.template.js

--- ansible/collections/mtp/common/roles/build_frontend/files/inject.template.js
+++ ansible/collections/mtp/common/roles/build_frontend/files/inject.template.js
@@ -1,5 +1,6 @@
 window.injectedEnv = {
     configUrlApi: '${CONFIG_URL_API}',
     configUrlApiBeta: '${CONFIG_URL_API_BETA}',
-    frontend_version: '${frontend_version}'
+    frontend_version: '${frontend_version}',
+    configVersion: '${config_version}'
 };

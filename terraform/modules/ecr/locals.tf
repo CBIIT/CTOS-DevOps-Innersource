0879ee32148f3bf5828c23bf2ff04aae801575af
Sowmya Karavadi <ksowmya442@gmail.com>
2023-06-08 17:05:05 -0400

    updated ECR for backend api

--- terraform/modules/ecr/locals.tf
+++ terraform/modules/ecr/locals.tf
@@ -1,4 +1,5 @@
 locals {
-  frontend_repository_name = "${var.app}-frontend-${terraform.workspace}"
-  backend_repository_name  = "${var.app}-backend-${terraform.workspace}"
+  frontend_repository_name    = "${var.app}-frontend-${terraform.workspace}"
+  backend_repository_name     = "${var.app}-backend-${terraform.workspace}"
+  backend_api_repository_name = "${var.app}-backend-api-${terraform.workspace}"
 }
--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -29,3 +29,19 @@ output "backend_repo_url" {
 output "backend_repo_name" {
   value = aws_ecr_repository.backend.name
 }
+
+output "backend_api_repo_arn" {
+  value = aws_ecr_repository.backend_api.arn
+}
+
+output "backend_api_registry_id" {
+  value = aws_ecr_repository.backend_api.registry_id
+}
+
+output "backend_api_repo_url" {
+  value = aws_ecr_repository.backend_api.repository_url
+}
+
+output "backend_api_repo_name" {
+  value = aws_ecr_repository.backend_api.name
+}

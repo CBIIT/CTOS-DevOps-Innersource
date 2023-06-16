15b06ffe1d8f41561079ec8dd57a39187158bd52
Sowmya Karavadi <ksowmya442@gmail.com>
2023-06-16 10:54:40 -0400

    updated typo

--- ansible/collections/mtp/common/roles/build_frontend_ot/files/inject.template.js
+++ ansible/collections/mtp/common/roles/build_frontend_ot/files/inject.template.js
@@ -2,6 +2,6 @@ window.injectedEnv = {
     configUrlApi: '${CONFIG_URL_API}',
     configUrlApiBeta: '${CONFIG_URL_API_BETA}',
     frontend_version: '${frontend_version}',
-    configVersion: '${config_version}'
+    configVersion: '${config_version}',
     chopRServer: '${frontend_url}'
 };
